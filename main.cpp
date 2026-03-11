ae22cdfd34fdca13e7843f8c29cd9afab249732f
mustafacelik-sag <mustafa.celik2528@hotmail.com>
2026-03-11 12:17:23 +0300

    final commit

--- main.cpp
+++ main.cpp
@@ -18,28 +18,37 @@ void Print(node *head){
         head = head->next;
     }
 }
-void Undo(node *head){
-    if(head != NULL){
-        Undo(head->next);
+node *Undo(node* head){
+    if(head == NULL) return NULL;
+    if(head -> next == NULL){
+        delete head;
+        return NULL;
     }
-    else delete head;
+    node *secondlast = head;
+    while(secondlast->next->next != NULL){
+        secondlast = secondlast -> next;
+    }
+    delete secondlast -> next;
+    secondlast->next = NULL;
+
+    return head;
 }
 int main()
 {
     string name;
     node *head = NULL;
     while(1 > 0){
+
+    cout << "Enter name:(Type EXIT to leave, UNDO to undo.)" << endl;
+    cin >> name;
+    if(name != "UNDO")head = Append(name, head);
     if(name == "EXIT"){
         cout << "Thanks for using our system." << endl;
         exit(0);
     }
-    cout << "Enter name:(Type EXIT to leave, UNDO to undo.)" << endl;
-    cin >> name;
-    head = Append(name, head);
     Print(head);
     if(name == "UNDO"){
-        Undo(head);
+        head = Undo(head);
     }
-
     }
 }
